3ab055782e803954eb1487ed19dbdb0884d33877
Mustafa Kemal GOKCE <mkemalgokce10.com>
2021-06-22 16:51:40 +0300

    -

--- client.c
+++ client.c
@@ -5,43 +5,73 @@
 
 */
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
 
 #include <arpa/inet.h>
-#include <netinet/in.h>
 
-int main (int argc, char **argv){
-    if(argc == 1){
-        printf("Lutfen port giriniz ! \n");
-        return 0;
+void send_file(int sock, int buffer_size){
+    // Dosya yollamaya yarayan fonksiyon.
+    send(sock,"dosya",128,0);
+    FILE *fp;
+    fp = fopen("a.jpg","rb");
+    char buffer[buffer_size];
+    while(fgets(buffer,buffer_size,fp) != NULL){
+        
+        if(send(sock, buffer, sizeof(buffer), 0 ) == -1){
+            perror("[-] Dosya yollanirken hata olustu !");
+            exit(EXIT_FAILURE);
+        }
+        memset(buffer, 0, buffer_size);
+        
     }
-    int portNumber = atoi(argv[1]);
+    if(send(sock, "bitti", sizeof("bitti"), 0 ) == -1){
+            perror("[-] Dosya yollanirken hata olustu !");
+            exit(EXIT_FAILURE);
+        }
+    fclose(fp);
+    return;
+}
+int main(int argc, char **argv){
+    
+    if (argc != 3){
+        printf("[!] Lutfen Port ve IP numarasini giriniz! \n");
+        exit(EXIT_FAILURE);
+    }
+    int port_number = atoi(argv[2]);
+    char *ip = argv[1];
+    char serverMessage[256] = "Servera baglandiniz. \n";
+    int status = 0;
     // Socket Olusturma
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock == -1) perror("sock");
+    if(sock == -1){
+        perror("[-] Socket olusturulurken hata olustu !");
+        exit(EXIT_FAILURE);
+    }
+    printf("[+] Socket olusturuldu. \n");
     // Socket icin degerlerin atanmasi
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(portNumber);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(port_number);
+    server_address.sin_addr.s_addr = inet_addr(ip);
     
     int con = connect(sock, (struct sockaddr *) &server_address, sizeof(server_address));
-    
+
     if (con == -1){
-        perror("con");
-        return 0;
+        perror("[-] Baglanti saglanamadi !");
+        exit(EXIT_FAILURE);
     }
+    printf("[+] Baglanti olusturuldu. \n");
     char server_response[256];
 
+    send_file(sock, 2048);
+    printf("[+] Dosya yollandi. \n");
     recv(sock, &server_response, sizeof(server_response), 0);
+    printf("Serverden gelen data : %s ", server_response);
 
-    printf("Server sent this data : %s \n", server_response);
-
-    close(sock);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -9,37 +9,88 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
 
-#include <netinet/in.h>
+#include <arpa/inet.h>
 
+void read_file(int sock, int buffer_size){
+    // Dosya okumaya yarayan fonksiyon.
+    FILE *fp = fopen("new_a.txt","wb");
+    printf("[..] Dosya okunuyor.");
+    char buffer[buffer_size];
+    while(1){
+        
+        recv(sock,buffer,buffer_size,0);
+        if(strcmp("bitti",buffer) == 0){
+            break;
+        }
+        fprintf(fp,"%s",buffer);
+        memset(buffer, 0, buffer_size);
+        
+    }
+    
+    fclose(fp);
+    return;
+}
 int main(int argc, char **argv){
 
-    if (argc == 1){
-        printf("Lutfen Port numarasini giriniz! \n");
-        return 0;
+    int port_number = atoi(argv[2]);
+    char *ip = argv[1];
+    char serverMessage[256] = "[!] Server kapaniyor... \n";
+    int status = 0;
+    char buffer[1024];
+    if (argc != 3){
+        printf("[!] Lutfen Port ve IP numarasini giriniz! \n");
+        exit(EXIT_FAILURE);
     }
-    int portNumber = atoi(argv[1]);
-    char serverMessage[256] = "Servera baglandiniz. \n";
     // Socket Olusturma
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock == -1 ) perror("server_sock");
+    if (server_sock == -1 ) {
+        perror("[-] Socket olustururken hata olustu !");
+        exit(EXIT_FAILURE);
+    }
+    printf("[+] Server socketi olusturuldu. \n");
     // Server adres tanimlama 
     struct sockaddr_in server_adress;
     server_adress.sin_family = AF_INET;
-    server_adress.sin_port = htons(portNumber);
-    server_adress.sin_addr.s_addr = INADDR_ANY;
+    server_adress.sin_port = htons(port_number);
+    server_adress.sin_addr.s_addr = inet_addr(ip);
 
     // Binding
-    bind(server_sock,(struct sockaddr*) &server_adress, sizeof(server_adress));
+    status = bind(server_sock,(struct sockaddr*) &server_adress, sizeof(server_adress));
+    if(status < 0){
+        perror("[-] Bind'larken hata olustu !");
+        exit(EXIT_FAILURE);
+    }
+    printf("[+] Bind islemi basarili. \n");
 
-    listen(server_sock, 5);
+    status = listen(server_sock, 5);
+    if(status < 0){
+        perror("[-] Listen islemi basarisiz !");
+        exit(EXIT_FAILURE);
+    }
+    printf("[+] Dinleniyor ... \n");
 
     int client_sock = accept(server_sock, NULL, NULL );
-
+    
+    while (1){
+        
+        recv(client_sock,buffer,128,0);
+        if(strcmp(buffer, "dosya") == 0){
+            
+            read_file(client_sock, 2048);
+            printf("[+] Okuma tamamlandi. \n");
+        }
+        else if(strcmp(buffer, "bitir") == 0 ){
+            break;
+        }
+        
+    }
+    
     send(client_sock, serverMessage, sizeof(serverMessage), 0);
-
+    
     close(server_sock);
-
+    
     return 0;
 
 }
